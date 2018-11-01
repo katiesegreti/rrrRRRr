3ed128b3156e1f8550126efa9642d989c87f4feb
Katie <katie.segreti@gmail.com>
2018-11-01 17:06:06 -0400

    Update unsupervised_learning.R

--- unsupervised_learning.R
+++ unsupervised_learning.R
@@ -1,4 +1,6 @@
 ## kmeans() is a base R function!
+library(readr)
+
 ?kmeans
 
 x <- read_csv("kmeans_x.csv")
@@ -54,15 +56,15 @@ plot(1:15, wss, type = "b",
 # Set k equal to the number of clusters corresponding to the elbow location
 k <- 2
 
-pokemon <- read_csv("Pokemon.csv")
-
+pokemon <- read_csv("https://assets.datacamp.com/production/course_1815/datasets/Pokemon.csv")
+pokemon2 <- pokemon[, -4 ]
 # Initialize total within sum of squares error: wss
 wss <- 0
 
 # Look over 1 to 15 possible clusters
 for (i in 1:15) {
   # Fit the model: km.out
-  km.out <- kmeans(pokemon, centers = i, nstart = 20, iter.max = 50)
+  km.out <- kmeans(pokemon2, centers = i, nstart = 20, iter.max = 50)
   # Save the within cluster sum of squares
   wss[i] <- km.out$tot.withinss
 }
@@ -93,3 +95,94 @@ hclust.out <- hclust(dist(x))
 
 # Inspect the result
 summary(hclust.out)
+
+
+# Cut by height
+cutree(hclust.out, h = 7)
+
+# Cut by number of clusters
+cutree(hclust.out, k = 3)
+
+# Cluster using complete linkage: hclust.complete
+hclust.complete <- hclust(dist(x), method = "complete")
+
+# Cluster using average linkage: hclust.average
+hclust.average <- hclust(dist(x), method = "average")
+
+# Cluster using single linkage: hclust.single
+hclust.single <- hclust(dist(x), method = "single")
+
+# Plot dendrogram of hclust.complete
+plot(hclust.complete, main = "Complete")
+
+# Plot dendrogram of hclust.average
+plot(hclust.average, main = "Average")
+
+# Plot dendrogram of hclust.single
+plot(hclust.single, main = "Single")
+
+pokemon3 <- pokemon2[, 4:11]
+###WTF is going on with pokemon notworking here?
+## oh it's the logical column stored as strings!
+colMeans(pokemon3)
+mean(pokemon$Total)
+colMeans(alien)
+
+# View column means
+colMeans(pokemon)
+
+# View column standard deviations
+apply(pokemon, 2, sd)
+
+# Scale the data
+pokemon.scaled <- scale(pokemon)
+
+# Create hierarchical clustering model: hclust.pokemon
+hclust.pokemon <- hclust(dist(pokemon.scaled), method = "complete")
+
+# Apply cutree() to hclust.pokemon: cut.pokemon
+cut.pokemon <- cutree(hclust.pokemon, k = 3)
+
+# Compare methods
+table(km.pokemon$cluster, cut.pokemon)
+
+#####PCA
+# Perform scaled PCA: pr.out
+pr.out <- prcomp(x = pokemon, scale = TRUE, center = TRUE)
+
+# Inspect model output
+summary(pr.out)
+
+# Variability of each principal component: pr.var
+pr.var <- pr.out$sdev^2
+
+# Variance explained by each principal component: pve
+# proportion of variance explained
+pve <- pr.var / sum(pr.var)
+
+# Plot variance explained for each principal component
+plot(pve, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
+
+# Plot cumulative proportion of variance explained
+plot(cumsum(pve), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
+
+###CASE STUDY
+url <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1903/datasets/WisconsinCancer.csv"
+
+# Download the data: wisc.df
+wisc.df <- read.csv(url)
+
+# Convert the features of the data: wisc.data
+wisc.data <- as.matrix(wisc.df[, 3:32])
+
+# Set the row names of wisc.data
+row.names(wisc.data) <- wisc.df$id
+
+# Create diagnosis vector
+diagnosis <- as.numeric(wisc.df$diagnosis == "M")
+
+
