4baead191870613fc53a5c4c5e876fcf19e0c1be
Katie <katie.segreti@gmail.com>
2018-11-21 12:47:42 -0500

    Update pixles.R to include thanksgiving leaf

--- pixels.R
+++ pixels.R
@@ -4,7 +4,7 @@ library(gridExtra)
 
 #blank theme
 pix_theme <- theme(panel.grid = element_blank(), 
-                       panel.background = element_rect(fill = "#707B7C"),
+                       panel.background = element_rect(fill = "white"),
                        axis.text = element_blank(), 
                        axis.ticks = element_blank(), 
                        axis.title = element_blank(),
@@ -181,3 +181,87 @@ alien_purple <- alien %>%
 grid.arrange(alien_green, alien_blue, alien_pink, alien_purple, nrow = 2, ncol = 2)
 
 
+#leaf
+x <- rep(1:27, each = 26)
+y <- rep(1:26, times = 27)
+c <- rep(0, 702)
+leaf <- data.frame(x, y, c)
+
+leaf$c[y == 1] <- c()
+leaf$c[y == 2] <- c()
+leaf$c[y == 3] <- c()
+leaf$c[y == 4] <- c()
+leaf$c[y == 5] <- c()
+leaf$c[y == 6] <- c(rep(0, 7), rep(1, 4), rep(2, 8), rep(1, 6), 0, 0)
+leaf$c[y == 7] <- c(rep(0, 5), rep(1, 6), rep(2, 12), 1, 0, 0, 0)
+leaf$c[y == 8] <- c(0, 0, 0, 1, 1, rep(3, 5), rep(1, 6), rep(2, 7), 1, 0, 0, 0)
+leaf$c[y == 9] <- c(0, 0, 1, 1, 4, 3, 3, 3, 3, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 0, 0)
+leaf$c[y == 10] <- c(0, 1, 4, 4, 4, 3, 3, 3, 3, 1, 3, 1, rep(2, 7), 1, 2, 2, 2, 2, 1, 1, 0)
+leaf$c[y == 11] <- c(1, 4, 4, 4, 4, 3, 3, 3, 1, 1, 3, 1, 1, rep(2, 12), 1, 0)  
+leaf$c[y == 12] <- c(1, 1, 1, 4, 4, 3, 3, 3, 1, 3, 3, 3, 1, rep(2, 11), 1, 1, 1)
+leaf$c[y == 13] <- c(0, 0, 0, 1, 4, 3, 3, 1, 1, 3, 3, 3, 1, 1, rep(2, 10), 1, 0, 0)
+leaf$c[y == 14] <- c(0, 0, 0, 1, 4, 3, 3, 1, 3, 3, 3, 3, 2, 1, rep(2, 5), rep(1, 5), 0, 0, 0)  
+leaf$c[y == 15] <- c(0, 0, 1, 1, 4, 3, 1, 1, 3, 3, 3, 3, 2, 2, 1, 2, 2, 2, 2, 1, rep(0,7))
+leaf$c[y == 16] <- c(0, 0, 1, 4, 4, 3, 1, rep(3, 5), 2, 2, 1, 1, 2, 2, 2, 1, rep(0, 7))
+leaf$c[y == 17] <- c(0, 1, 4, 4, 4, rep(3, 7), 2, 2, 2, 1, 2, 2, 2, 1, 1, rep(0, 6))
+leaf$c[y == 18] <- c(0, 1, 1, 1, 4, 4, rep(3, 7), 2, 2, 1, 1, 2, 2, 2, 1, 1, rep(0, 5))
+leaf$c[y == 19] <- c(0, 0, 0, 1, 4, 4, 3, 3, 3, 1, 1, 1, 3, rep(2, 8), 1, 1, 0, 0, 0, 0)
+leaf$c[y == 20] <- c(0, 0, 0, 1, 4, 4, 3, 3, 3, 1, 0, 1, 1, rep(2, 7), 1, 1, 1, 0, 0, 0, 0)
+leaf$c[y == 21] <- c(0, 0, 0, 1, 4, 3, 3, 3, 1, 0, 0, 0, 1, rep(2, 7), 1, rep(0, 6))
+leaf$c[y == 22] <- c(0, 0, 0, 1, 1, 3, 1, 1, 1, 0, 0, 0, 1, 1, rep(2, 6), 1, rep(0, 6))
+leaf$c[y == 23] <- c(0, 0, 0, 0, 1, 1, 1, rep(0, 6), 1, rep(2, 5), 1, 1, rep(0, 6))
+leaf$c[y == 24] <- c(0, 0, 0, 0, 1, 1, rep(0, 7), rep(1, 5), 2, 1, 1, rep(0, 6))
+leaf$c[y == 25] <- c(rep(0, 13), 1, 1, 0, 0, 1, 1, 1, 1, rep(0, 6))
+leaf$c[y == 26] <- c(rep(0, 19), 1, 1, rep(0, 6))
+
+
+leaf1 %>%
+  filter(c > 0) %>%
+  ggplot(aes(x, y, fill = factor(c))) +
+  geom_point(size = 8, shape = 22) +
+  scale_fill_manual(values = c("brown", "red", "orange", "yellow")) +
+  pix_theme +
+  xlim(1, 27) +
+  ylim(1, 26)
+
+leaf %>%
+  filter(c > 0) %>%
+  ggplot(aes(x, y, fill = factor(c))) +
+  geom_point(size = 8, shape = 22) +
+  scale_fill_manual(values = c("brown", "red", "orange", "yellow")) +
+  pix_theme +
+  xlim(1, 27) +
+  ylim(1, 26)
+
+#leaf
+x <- rep(1:27, each = 26)
+y <- rep(1:26, times = 27)
+c <- rep(0, 702)
+leaf1 <- data.frame(x, y, c)
+
+leaf1$c[y == 1] <- c(0, 0, 0, 1, 1, 1, rep(0, 21))
+leaf1$c[y == 2] <- c(0, 0, 0, 1, 1, 1, 1, rep(0, 10), 1, 1, rep(0, 8))
+leaf1$c[y == 3] <- c(rep(0, 5), 1, 1, 1, rep(0, 6), rep(1, 5), rep(0, 8))
+leaf1$c[y == 4] <- c(rep(0, 6), 1, 1, 1, 0, 0, 0, 1, 1, 2, 2, 2, 1, rep(0,9))
+leaf1$c[y == 5] <- c(rep(0, 7), rep(1, 5), rep(2, 5), 1, 1, 1, rep(0, 7))
+leaf1$c[y == 6] <- c(rep(0, 7), rep(1, 4), rep(2, 8), rep(1, 6), 0, 0)
+leaf1$c[y == 7] <- c(rep(0, 5), rep(1, 6), rep(2, 12), 1, 0, 0, 0)
+leaf1$c[y == 8] <- c(0, 0, 0, 1, 1, rep(3, 5), rep(1, 6), rep(2, 7), 1, 0, 0, 0)
+leaf1$c[y == 9] <- c(0, 0, 1, 1, 4, 3, 3, 3, 3, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 0, 0)
+leaf1$c[y == 10] <- c(0, 1, 4, 4, 4, 3, 3, 3, 3, 1, 3, 1, rep(2, 7), 1, 2, 2, 2, 2, 1, 1, 0)
+leaf1$c[y == 11] <- c(1, 4, 4, 4, 4, 3, 3, 3, 1, 1, 3, 1, 1, rep(2, 12), 1, 0)  
+leaf1$c[y == 12] <- c(1, 1, 1, 4, 4, 3, 3, 3, 1, 3, 3, 3, 1, rep(2, 11), 1, 1, 1)
+leaf1$c[y == 13] <- c(0, 0, 0, 1, 4, 3, 3, 1, 1, 3, 3, 3, 1, 1, rep(2, 10), 1, 0, 0)
+leaf1$c[y == 14] <- c(0, 0, 0, 1, 4, 3, 3, 1, 3, 3, 3, 3, 2, 1, rep(2, 5), rep(1, 5), 0, 0, 0)  
+leaf1$c[y == 15] <- c(0, 0, 1, 1, 4, 3, 1, 1, 3, 3, 3, 3, 2, 2, 1, 2, 2, 2, 2, 1, rep(0,7))
+leaf1$c[y == 16] <- c(0, 0, 1, 4, 4, 3, 1, rep(3, 5), 2, 2, 1, 1, 2, 2, 2, 1, rep(0, 7))
+leaf1$c[y == 17] <- c(0, 1, 4, 4, 4, rep(3, 7), 2, 2, 2, 1, 2, 2, 2, 1, 1, rep(0, 6))
+leaf1$c[y == 18] <- c(0, 1, 1, 1, 4, 4, rep(3, 7), 2, 2, 1, 1, 2, 2, 2, 1, 1, rep(0, 5))
+leaf1$c[y == 19] <- c(0, 0, 0, 1, 4, 4, 3, 3, 3, 1, 1, 1, 3, rep(2, 8), 1, 1, 0, 0, 0, 0)
+leaf1$c[y == 20] <- c(0, 0, 0, 1, 4, 4, 3, 3, 3, 1, 0, 1, 1, rep(2, 7), 1, 1, 1, 0, 0, 0, 0)
+leaf1$c[y == 21] <- c(0, 0, 0, 1, 4, 3, 3, 3, 1, 0, 0, 0, 1, rep(2, 7), 1, rep(0, 6))
+leaf1$c[y == 22] <- c(0, 0, 0, 1, 1, 3, 1, 1, 1, 0, 0, 0, 1, 1, rep(2, 6), 1, rep(0, 6))
+leaf1$c[y == 23] <- c(0, 0, 0, 0, 1, 1, 1, rep(0, 6), 1, rep(2, 5), 1, 1, rep(0, 6))
+leaf1$c[y == 24] <- c(0, 0, 0, 0, 1, 1, rep(0, 7), rep(1, 5), 2, 1, 1, rep(0, 6))
+leaf1$c[y == 25] <- c(rep(0, 13), 1, 1, 0, 0, 1, 1, 1, 1, rep(0, 6))
+leaf1$c[y == 26] <- c(rep(0, 19), 1, 1, rep(0, 6))
